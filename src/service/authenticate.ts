e847e9b1e6c3b8d3a086fd13d61e90fe13c336c8
danielqueirozc <danielqueiroz.job@gmail.com>
2025-06-19 21:28:34 -0300

    feat: create authenticate service

---
+++ src/service/authenticate.ts
@@ -0,0 +1,34 @@
+import { User } from "@prisma/client";
+import { UsersRepository } from "../repositories/users-repository";
+import { compare } from "bcryptjs";
+
+interface AuthenticateServiceRequest {
+    email: string;
+    password: string;
+}
+
+interface AuthenticateServiceResponse {
+    user: User;
+}
+
+export class AuthenticateService {
+    constructor(private usersRepository: UsersRepository) {}
+
+    async execute({ email, password }: AuthenticateServiceRequest): Promise<AuthenticateServiceResponse> {
+        const user = await this.usersRepository.findByEmail(email)
+
+        if (!user) {
+            throw new Error('Invalid credentials.')
+        }
+
+        const doesPasswordMatches = await compare(password, user.password_hash)
+
+        if (!doesPasswordMatches) {
+            throw new Error('Invalid credentials.')
+        }
+
+        return {
+            user,
+        }
+    }
+}
