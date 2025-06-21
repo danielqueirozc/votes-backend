c03bee898e8f9b2f9c65954ccb856a76d17adbaf
danielqueirozc <danielqueiroz.job@gmail.com>
2025-06-21 18:24:38 -0300

    feat: add try catch

--- src/http/controller/register.ts
+++ src/http/controller/register.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
+import { MakeRegisterService } from "../../service/factories/make-register-service";
 
 export function Register(request: FastifyRequest, reply: FastifyReply) {
     const bodyZodSchema = z.object({
@@ -9,5 +10,15 @@ export function Register(request: FastifyRequest, reply: FastifyReply) {
     })
 
     const { name, email, password } = bodyZodSchema.parse(request.body)
+
+    try {
+        const registerService = MakeRegisterService()
+
+        registerService.execute({ name, email, password })
+    } catch (error) {
+        if (error instanceof Error) {
+            return reply.status(400).send({ message: error.message })
+        }
+    }
 
 }
